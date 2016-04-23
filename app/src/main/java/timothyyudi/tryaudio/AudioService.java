f70f3380cc0b6356854aacab9cb4e7fef8259e73
Timothy Yudi <timothyyudi@gmail.com>
2016-04-23 16:08:03 +0700

    todo: step 3 from section 3

---
+++ app/src/main/java/timothyyudi/tryaudio/AudioService.java
@@ -0,0 +1,81 @@
+package timothyyudi.tryaudio;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+
+public class AudioService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+
+    private MediaPlayer player;
+    private String audioURI;
+    private final IBinder audioBind = new AudioBinder();
+
+    public AudioService() {}
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return audioBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = new MediaPlayer(); //create player
+        initAudioPlayer();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    public void initAudioPlayer(){  //set player properties
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void playSong(){
+        player.reset();
+        try{player.setDataSource(getApplicationContext(), Uri.parse(audioURI));}
+        catch(Exception e){Log.e("AUIDO SERVICE", "Error setting data source", e);}
+        player.prepareAsync();
+    }
+
+    public void setAudioURI(String audioURI) {
+        this.audioURI = audioURI;
+    }
+
+    public class AudioBinder extends Binder { //will be accessed from activity
+
+        AudioService getService() {
+            return AudioService.this;
+        }
+
+    }
+}
--- app/src/main/java/timothyyudi/tryaudio/MainActivity.java
+++ app/src/main/java/timothyyudi/tryaudio/MainActivity.java
@@ -1,13 +1,69 @@
 package timothyyudi.tryaudio;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAudio1,btnAudio2;
+
+    private AudioService audioSrv;
+    private Intent playIntent;
+    private boolean audioBound=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAudio1 = (Button) findViewById(R.id.btnAudio1);
+        btnAudio1.setOnClickListener(this);
+        btnAudio2 = (Button) findViewById(R.id.btnAudio2);
+        btnAudio2.setOnClickListener(this);
+    }
+
+    //connect to the service
+    private ServiceConnection audioConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            AudioService.AudioBinder binder = (AudioService.AudioBinder)service;
+            //get service
+            audioSrv = binder.getService();
+            //pass list
+            audioSrv.setAudioURI();
+            audioBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            audioBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this, AudioService.class);
+            bindService(playIntent, audioConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnAudio1){
+
+        }else if(v.getId()==R.id.btnAudio2){
+
+        }
     }
 }
