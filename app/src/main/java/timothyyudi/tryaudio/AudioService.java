2f2d6cd30a417c0154627268237f568b6a42bc1f
Timothy Yudi <timothyyudi@gmail.com>
2016-04-27 06:55:29 +0700

    simp[lify

--- app/src/main/java/timothyyudi/tryaudio/AudioService.java
+++ app/src/main/java/timothyyudi/tryaudio/AudioService.java
@@ -10,7 +10,7 @@ import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
 
-public class AudioService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+public class AudioService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnInfoListener, MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnSeekCompleteListener {
 
     private MediaPlayer player;
     private String audioURI;
@@ -34,7 +34,15 @@ public class AudioService extends Service implements MediaPlayer.OnPreparedListe
     public void onCreate() {
         super.onCreate();
         player = new MediaPlayer(); //create player
-        initAudioPlayer();
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+        player.setOnBufferingUpdateListener(this);
+        player.setOnSeekCompleteListener(this);
+        player.setOnInfoListener(this);
+        player.reset();
     }
 
     @Override
@@ -52,23 +60,30 @@ public class AudioService extends Service implements MediaPlayer.OnPreparedListe
         mp.start();
     }
 
-    public void initAudioPlayer(){  //set player properties
-        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        player.setOnPreparedListener(this);
-        player.setOnCompletionListener(this);
-        player.setOnErrorListener(this);
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+    }
+
+    @Override
+    public void onSeekComplete(MediaPlayer mp) {
+
+    }
+
+    public void setAudioURI(String audioURI) {
+        this.audioURI = audioURI;
     }
 
     public void playAudio(){
         player.reset();
         try{player.setDataSource(getApplicationContext(), Uri.parse(audioURI));}
-        catch(Exception e){Log.e("AUIDO SERVICE", "Error setting data source", e);}
+        catch(Exception e){Log.e("AUDIO SERVICE", "Error setting data source", e);}
         player.prepareAsync();
-    }
-
-    public void setAudioURI(String audioURI) {
-        this.audioURI = audioURI;
     }
 
     public class AudioBinder extends Binder { //will be accessed from activity
